3e8169661c771f9a9c81318c85d91c1718b71312
Pirot1 <t00707762@gmail.com>
2024-04-12 21:05:23 +0500

    another update

---
+++ script.js
@@ -0,0 +1,7 @@
+let main = document.querySelector('.main')
+let header = document.querySelector('.header')
+
+setTimeout(function(){
+    header.style.display = 'none'
+    main.style.display = 'flex'
+}, 3000);
